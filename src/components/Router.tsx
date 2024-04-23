76a766b8daee2719d02baccd7b61b61848ec31d4
이정섭 <matthew@jslee.local>
2024-04-23 16:29:36 +0900

    feat: profile, post components

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes, Navigate} from "react-router-dom"
 import Home from "../pages/home";
-import PostList from "../pages/posts";
-import PostDetail from "../pages/posts/detail";
+import PostsPage from "../pages/posts";
+import PostPage from "../pages/posts/detail";
 import PostNew from "../pages/posts/new";
 import PostEdit from "../pages/posts/edit";
-import Profile from "../pages/profile";
+import ProfilePage from "../pages/profile";
 import Login from "../pages/login";
 import Signup from "../pages/signup";
 
@@ -12,11 +12,11 @@ function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />}/>
-      <Route path="/posts" element={<PostList />}/>
-      <Route path="/posts/:id" element={<PostDetail />}/>
+      <Route path="/posts" element={<PostsPage />}/>
+      <Route path="/posts/:id" element={<PostPage />}/>
       <Route path="/posts/new" element={<PostNew />}/>
       <Route path="/posts/edit/:id" element={<PostEdit />}/>
-      <Route path="/profile" element={<Profile />}/>
+      <Route path="/profile" element={<ProfilePage />}/>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="*" element={<Navigate replace to ='/'/>}/>
