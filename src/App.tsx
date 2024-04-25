d3685ef3558e1be26c0e1eef573eef2937d99906
이정섭 <matthew@jslee.local>
2024-04-25 17:18:24 +0900

    feat: 로그인 실습

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
+import { useEffect, useState } from "react";
 import Router from "components/Router";
 import { app } from "firebaseApp";
-import { getAuth } from "firebase/auth";
-import { useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
+import Loader from "components/Loader";
 
 function App() {
   const auth = getAuth(app);
 
+  const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <>
       <ToastContainer />
-      <Router isAuthenticated={isAuthenticated} />;
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />};
     </>
   );
 }
